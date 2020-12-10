ef0502735025c6f8bb709a0261794036c237d09b
Josh Bailey <josh@vandervecken.com>
2020-12-10 20:28:04 +0000

    compile only int test.

--- notnicetcp.py
+++ notnicetcp.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
+import argparse
 from bcc import BPF
-import time
 
-device = "lo"
-print('initializing')
+parser = argparse.ArgumentParser(description='notnicetcp')
+parser.add_argument('--device', type=str)
+parser.add_argument('--attach', action='store_true')
+parser.add_argument('--no-attach', dest='attach', action='store_false')
+parser.set_defaults(attach=True)
+args = parser.parse_args()
+
 b = BPF(src_file='notnicetcp.c')
 fn = b.load_func('notnicetcp', BPF.XDP)
-b.attach_xdp(device, fn, 0)
-print('attached')
-
-try:
-  b.trace_print()
-except KeyboardInterrupt:
-  pass
+if args.attach:
+    b.attach_xdp(args.device, fn, 0)
+    try:
+        b.trace_print()
+    except KeyboardInterrupt:
+        pass

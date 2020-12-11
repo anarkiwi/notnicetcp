6939d720d2daeec6cbc9543ab38924561d5f76bd
Josh Bailey <josh@vandervecken.com>
2020-12-11 01:21:26 +0000

    handle VLANs.

--- notnicetcp.c
+++ notnicetcp.c
@@ -15,23 +15,36 @@
 int notnicetcp(struct xdp_md *ctx) {
   void *data = (void*)(long)ctx->data;
   void *data_end = (void*)(long)ctx->data_end;
-  // TODO: handle VLAN
   struct ethhdr *eth = data;
-  if ((void*)eth + sizeof(*eth) + sizeof(struct iphdr) <= data_end) {
-    struct iphdr *ip = data + sizeof(*eth);
-    u16 tot_len = ntohs(ip->tot_len);
-    if (ip->protocol == IPPROTO_TCP && (void*)ip + sizeof(*ip) + sizeof(struct tcphdr) <= data_end) {
-      struct tcphdr *tcp = (void*)ip + sizeof(*ip);
-      u16 tcp_header_size = tcp->doff << 2;
-      // Look for a GET, and maybe break the URL.
-      if ((void*)tcp + tcp_header_size + 8 <= data_end && tot_len > 52) {
-	unsigned char *payload = (void*)tcp + tcp_header_size;
-	if (payload[0] == 'G' && payload[1] == 'E' && payload[2] == 'T') {
-          bpf_trace_printk("got a GET\n");
-	  u32 dice = bpf_get_prandom_u32();
-	  if (dice > 2147483648) {
-	    payload[4] = dice & 255;
-	    bpf_trace_printk("broke the GET\n");
+  if ((void*)eth + sizeof(*eth) <= data_end) {
+    u16 eth_type = ntohs(eth->h_proto);
+    struct iphdr *ip = NULL;
+    if (eth_type == ETH_P_IP) {
+      ip = data + sizeof(*eth);
+    } else if (eth_type == ETH_P_8021Q || eth_type == ETH_P_8021AD) {
+      if ((void*)eth + sizeof(*eth) + sizeof(struct vlan_hdr) <= data_end) {
+        struct vlan_hdr *vlan_hdr = (void*)eth + sizeof(*eth);
+	eth_type = ntohs(vlan_hdr->h_vlan_encapsulated_proto);
+	if (eth_type == ETH_P_IP) {
+	  ip = (struct iphdr*)((void*)vlan_hdr + sizeof(*vlan_hdr));
+	}
+      }
+    }
+    if (ip) {
+      u16 tot_len = ntohs(ip->tot_len);
+      if (ip->protocol == IPPROTO_TCP && (void*)ip + sizeof(*ip) + sizeof(struct tcphdr) <= data_end) {
+        struct tcphdr *tcp = (void*)ip + sizeof(*ip);
+        u16 tcp_header_size = tcp->doff << 2;
+        // Look for a GET, and maybe break the URL.
+        if ((void*)tcp + tcp_header_size + 8 <= data_end && tot_len > 52) {
+          unsigned char *payload = (void*)tcp + tcp_header_size;
+	  if (payload[0] == 'G' && payload[1] == 'E' && payload[2] == 'T') {
+            bpf_trace_printk("got a GET\n");
+	    u32 dice = bpf_get_prandom_u32();
+	    if (dice > 2147483648) {
+	      payload[4] = dice & 255;
+	      bpf_trace_printk("broke the GET\n");
+	    }
 	  }
 	}
       }
